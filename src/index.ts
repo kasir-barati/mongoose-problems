6a045aedf693513bc899bf79d7ed7b48423e8ada
Kasir <node.js.developers.kh@gmail.com>
2021-09-29 10:58:26 +0330

    bug: can not update discriminator fields

--- src/index.ts
+++ src/index.ts
@@ -6,59 +6,121 @@ dotenv.config({
 });
 
 import { connectToMongoDB } from './configs/mongodb.config';
-import { UserModel } from './schemas/user.schema';
+import {
+    UserModel,
+    userDiscriminatorKey,
+    UserDiscriminator,
+} from './schemas/user.schema';
 
 (async () => {
     await connectToMongoDB();
 
-    for (let index = 1; index < 5; index++) {
-        console.log(
-            await UserModel.create({
-                name: 'Kasir' + index,
-                email: 'temp@temp.com' + index,
+    console.log(
+        'guest user \n\r\n\r',
+        await UserModel.create({
+            name: 'Guest Kasir',
+            email: 'guest@temp.com',
+            profile: {
+                avatar: 'guest',
+                bio: 'unknown',
+            },
+            [userDiscriminatorKey]: UserDiscriminator.guest,
+            ip: '192.168.1.1',
+            activity: {
+                page: 'about-us',
+                exitedDate: new Date('2000'),
+                enteredDate: new Date('2020'),
+            },
+        }),
+    );
+    console.log(
+        'admin user \n\r\n\r',
+        await UserModel.create({
+            name: 'Admin Kasir',
+            email: 'admin@temp.com',
+            profile: {
+                avatar: 'admin',
+                bio: 'unknown',
+            },
+            [userDiscriminatorKey]: UserDiscriminator.admin,
+            password: 'hashed password',
+            employeeInfo: {
+                nationalId: '123456789',
+                employeeId: '987654321',
+            },
+        }),
+    );
+
+    console.log(
+        '\n\r\n\r',
+        'update with "field-name.sub-field-name: value"',
+        '\n\r\n\r',
+    );
+
+    await UserModel.updateOne(
+        { email: 'guest@temp.com' },
+        {
+            $set: {
+                [userDiscriminatorKey]: UserDiscriminator.guest,
+                'profile.avatar': 'custom new avatar',
+                'activity.page': 'index',
+                'ip': '195.86.5.123',
+            },
+        },
+    );
+    console.log(await find('guest@temp.com'));
+
+    await UserModel.updateOne(
+        { email: 'admin@temp.com' },
+        {
+            $set: {
+                [userDiscriminatorKey]: UserDiscriminator.admin,
+                'name': 'kasir san',
+                'profile.avatar': 'third new avatar',
+                'password': 'new hashed pass',
+                'employeeInfo.employeeId': 'another employee id set',
+            },
+        },
+    );
+    console.log(await find('admin@temp.com'));
+
+    console.log(
+        '\n\r\n\r',
+        'update with "field-name: {sub-field-name: value}"',
+        '\n\r\n\r',
+    );
+
+    await UserModel.updateOne(
+        { email: 'guest@temp.com' },
+        {
+            $set: {
+                name: 'kasir san name updated',
+                [userDiscriminatorKey]: UserDiscriminator.guest,
                 profile: {
-                    avatar: 'none',
-                    bio: 'nodejs developer',
+                    avatar: 'updated avatar',
                 },
-            }),
-        );
-    }
-
-    for (let index = 1; index < 5; index++) {
-        await UserModel.updateOne(
-            { email: 'temp@temp.com' + index },
-            {
-                $set: {
-                    'profile.avatar': 'second new avatar',
-                },
+                activity: { page: 'index' },
+                ip: '195.86.5.123',
             },
-        );
-        console.log(await find('temp@temp.com' + index));
+        },
+    );
+    console.log(await find('guest@temp.com'));
 
-        await UserModel.updateOne(
-            { email: 'temp@temp.com' + index },
-            {
-                $set: {
-                    'name': 'kasir san',
-                    'profile.avatar': 'third new avatar',
+    await UserModel.updateOne(
+        { email: 'admin@temp.com' },
+        {
+            $set: {
+                name: 'kasir san name updated',
+                [userDiscriminatorKey]: UserDiscriminator.admin,
+                profile: {
+                    avatar: 'admin update his/her avatar',
                 },
+                employeeInfo: { nationalId: 'admin new national id' },
+                password: '195.86.5.123 changes',
             },
-        );
-        console.log(await find('temp@temp.com' + index));
-
-        await UserModel.updateOne(
-            { email: 'temp@temp.com' + index },
-            {
-                $set: {
-                    name: 'kasir san',
-                    profile: {
-                        avatar: 'first new avatar',
-                    },
-                },
-            },
-        );
-        console.log(await find('temp@temp.com' + index));
-    }
+        },
+    );
+    console.log(await find('admin@temp.com'));
 })();
 
 function find(email: string) {
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,11 +1,31 @@
 import { model, Schema, Document } from 'mongoose';
 
+export const userDiscriminatorKey = 'userType';
+export enum UserDiscriminator {
+    admin = 'admin',
+    guest = 'guest',
+}
 export interface UserDocument extends Document {
     name?: string;
     email: string;
     profile?: {
         bio?: string;
         avatar?: string;
+    };
+}
+export interface GuestDocument extends Document {
+    ip: string;
+    activity: {
+        page: string;
+        exitedDate: Date;
+        enteredDate: Date;
+    };
+}
+export interface AdminDocument extends Document {
+    password: string;
+    employeeInfo: {
+        nationalId: string;
+        employeeId: string;
     };
 }
 
@@ -20,7 +40,26 @@ const userSchema = new Schema<UserDocument>(
     },
     {
         timestamps: true,
+        discriminatorKey: userDiscriminatorKey,
     },
 );
+const adminSchema = new Schema<AdminDocument>({
+    password: String,
+    employeeInfo: {
+        nationalId: String,
+        employeeId: String,
+    },
+});
+const guestSchema = new Schema<GuestDocument>({
+    ip: String,
+    activity: {
+        page: String,
+        exitedDate: Date,
+        enteredDate: Date,
+    },
+});
 
 export const UserModel = model<UserDocument>('user', userSchema);
+
+UserModel.discriminator(UserDiscriminator.admin, adminSchema);
+UserModel.discriminator(UserDiscriminator.guest, guestSchema);
